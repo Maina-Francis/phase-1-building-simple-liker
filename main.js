4d07c3362356a68ed0ad45251366cc227a0dc506
Francis Maina <sebastianfrancix@gmail.com>
2022-08-30 04:14:33 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
 
+const errorModel = document.getElementById("modal");
+errorModel.classList.add("hidden");
 
-
+const heartClicked = (event) => {
+  mimicServerCall()
+    .then(() => {
+      if (event.target.textContent == EMPTY_HEART) {
+        event.target.textContent = FULL_HEART;
+        event.target.classList.add("activated-heart");
+      } else {
+        event.target.textContent = EMPTY_HEART;
+        event.target.classList.remove("activated-heart");
+      }
+    })
+    .catch(() => {
+      errorModel.classList.remove("hidden");
+      setTimeout(function () {
+        errorModel.classList.add("hidden");
+      }, 3000);
+    });
+};
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
+let hearts = document.getElementsByClassName("like-glyph");
+for (let heart of hearts) {
+  heart.addEventListener("click", heartClicked);
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+  function mimicServerCall(
+    url = "http://mimicServer.example.com",
+    config = {}
+  ) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        let isRandomFailure = Math.random() < 0.2;
+        if (isRandomFailure) {
+          reject("Random server error. Try again.");
+        } else {
+          resolve("Pretend remote server notified of action!");
+        }
+      }, 300);
+    });
+  }
 }
